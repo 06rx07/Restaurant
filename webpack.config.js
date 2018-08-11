5bedd04ddcdbebddf8947a2be215b7668af18b87
Rachel Xi <rachelxi@Rachels-MacBook-Pro.local>
2018-08-12 00:03:18 +1200

    Add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        models: './scripts/models/index.ts'
+    },
+    output: {
+        path: _resolve('dist'),
+        filename: '[name].bundle.js'
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.jsx']
+    },
+    // devtool: 'source-map',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: 'awesome-typescript-loader'
+            }
+        ]
+    }
+};
+
+function _resolve(_path) {
+    return path.resolve(__dirname, _path);
+}
