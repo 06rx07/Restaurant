cbe555daa011bba1c3f12c84299205189dea4687
Rachel Xi <rachelxi@Rachels-MacBook-Pro.local>
2018-08-12 18:45:29 +1200

    Add dishes

---
+++ data/dishes.const.ts
@@ -0,0 +1,47 @@
+export const dishes = [
+    {
+        name: 'Bacon Backfire',
+        cost: 7.00,
+        price: 13.20
+    },
+    {
+        name: 'Thunderbird',
+        cost: 5.00,
+        price: 10.90
+    },
+    {
+        name: 'Burnout',
+        cost: 7.10,
+        price: 13.90
+    },
+    {
+        name: 'Chook Royale',
+        cost: 4.00,
+        price: 7.50
+    },
+    {
+        name: 'Smash Browns With Aioli',
+        cost: 2.00,
+        price: 5.90
+    },
+    {
+        name: 'Spud Fries With Aioli',
+        cost: 1.90,
+        price: 4.90
+    },
+    {
+        name: 'Chicken Fenders',
+        cost: 4.00,
+        price: 7.90
+    },
+    {
+        name: 'Knuckle Berry Spin',
+        cost: 2.50,
+        price: 5.50
+    },
+    {
+        name: 'Thickshake',
+        cost: 2.00,
+        price: 5.90
+    },
+];
