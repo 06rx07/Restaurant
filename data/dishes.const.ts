3604a344dac598d0021de1b9dc1665eb8902fafe
Rachel Xi <rachelxi@Rachels-MacBook-Pro.local>
2018-08-20 23:40:52 +1200

    Add data and config

--- data/dishes.const.ts
+++ data/dishes.const.ts
@@ -2,46 +2,55 @@ export const dishes = [
     {
         name: 'Bacon Backfire',
         cost: 7.00,
-        price: 13.20
+        price: 13.20,
+        cookTime: 12
     },
     {
         name: 'Thunderbird',
         cost: 5.00,
-        price: 10.90
+        price: 10.90,
+        cookTime: 10
     },
     {
         name: 'Burnout',
         cost: 7.10,
-        price: 13.90
+        price: 13.90,
+        cookTime: 12
     },
     {
         name: 'Chook Royale',
         cost: 4.00,
-        price: 7.50
+        price: 7.50,
+        cookTime: 5
     },
     {
         name: 'Smash Browns With Aioli',
         cost: 2.00,
-        price: 5.90
+        price: 5.90,
+        cookTime: 3
     },
     {
         name: 'Spud Fries With Aioli',
         cost: 1.90,
-        price: 4.90
+        price: 4.90,
+        cookTime: 3
     },
     {
         name: 'Chicken Fenders',
         cost: 4.00,
-        price: 7.90
+        price: 7.90,
+        cookTime: 5
     },
     {
         name: 'Knuckle Berry Spin',
         cost: 2.50,
-        price: 5.50
+        price: 5.50,
+        cookTime: 2
     },
     {
         name: 'Thickshake',
         cost: 2.00,
-        price: 5.90
+        price: 5.90,
+        cookTime: 5
     },
 ];
---
+++ scripts/settings.const.ts
@@ -0,0 +1,5 @@
+export const config = {
+    timeUnit: 1000,
+    eatTime: 3,
+    orderTime: 3,
+};
